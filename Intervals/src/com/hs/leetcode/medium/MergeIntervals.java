a7ede3248b76ec7f20dce353fb9f1e6aaa3e98fc
Hareram Singh <hareram.singh@netcracker.com>
2023-12-21 14:28:33 +0530

    Latest

--- Intervals/src/com/hs/leetcode/medium/MergeIntervals.java
+++ Intervals/src/com/hs/leetcode/medium/MergeIntervals.java
@@ -6,23 +6,27 @@ import java.util.List;
 
 public class MergeIntervals {
 	public int[][] merge(int[][] intervals) {
-		List<int[]> ans = new ArrayList<>();
-		Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-		ans.add(intervals[0]);
-		for (int i = 1; i < intervals.length; i++) {
-			// comparing the values of prevEnd and curStart
-			int curStart = intervals[i][0];
-			int[] prevEnd = ans.get(ans.size() - 1);
-			if (prevEnd[1] >= curStart) {
-				// do the merging
-				prevEnd[1] = Math.max(prevEnd[1], intervals[i][1]);
+		List<int[]> res = new ArrayList<>();
+
+		if (intervals.length == 0 || intervals == null)
+			return res.toArray(new int[0][]);
+
+		Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+		int start = intervals[0][0];
+		int end = intervals[0][1];
+
+		for (int[] i : intervals) {
+			if (i[0] <= end) {
+				end = Math.max(end, i[1]);
 			} else {
-				ans.add(intervals[i]);
+				res.add(new int[] { start, end });
+				start = i[0];
+				end = i[1];
 			}
 		}
-		int[][] res = new int[ans.size()][2];
-		ans.toArray(res);
-		return res;
+		res.add(new int[] { start, end });
+		return res.toArray(new int[0][]);
 	}
 
 	public static void main(String[] args) {
