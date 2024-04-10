fbaeb9a14071eef9fe4bd2fd5dd101a22d201c46
Hareram Singh <hareram.singh@netcracker.com>
2024-04-10 22:56:48 +0530

    Latest

--- Heap/src/com/hs/medium/HandOfStraights.java
+++ Heap/src/com/hs/medium/HandOfStraights.java
@@ -5,6 +5,9 @@ import java.util.Queue;
 
 public class HandOfStraights {
 	public boolean isNStraightHand(int[] hand, int groupSize) {
+		if (hand.length % groupSize != 0)
+			return false;
+		
 		Queue<Integer> minHeap = new PriorityQueue<>();
 		for (int i : hand) {
 			minHeap.add(i);
@@ -12,6 +15,9 @@ public class HandOfStraights {
 
 		while (!minHeap.isEmpty()) {
 			int first = minHeap.poll();
+
+			// it checks next consecutive groupSize cards
+			// if its not present it means its not possible to create a group
 			for (int i = 1; i < groupSize; i++) {
 				if (!minHeap.remove(first + i)) {
 					return false;
@@ -19,5 +25,13 @@ public class HandOfStraights {
 			}
 		}
 		return true;
+	}
+
+	public static void main(String[] args) {
+		HandOfStraights obj = new HandOfStraights();
+		int[] hand = { 1, 2, 3, 6, 2, 3, 4, 7, 8 };
+		int groupSize = 3;
+		boolean result = obj.isNStraightHand(hand, groupSize);
+		System.out.println(result);
 	}
 }
