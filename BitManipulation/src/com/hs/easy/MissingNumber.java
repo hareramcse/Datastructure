d92c41bc46e754d33d8e5576733636a732c8f126
Hareram Singh <hareram.singh@netcracker.com>
2024-12-12 00:08:08 +0530

    latest

--- BitManipulation/src/com/hs/easy/MissingNumber.java
+++ BitManipulation/src/com/hs/easy/MissingNumber.java
@@ -1,5 +1,16 @@
 package com.hs.easy;
 
 public class MissingNumber {
+	public int missingNumber(int[] nums) {
+		int missingNumber = 0;
+		for (int num : nums) {
+			missingNumber = missingNumber ^ num;
+		}
 
+		for (int i = 0; i <= nums.length; i++) {
+			missingNumber = missingNumber ^ i;
+		}
+
+		return missingNumber;
+	}
 }
