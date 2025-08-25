cdf9d241f2bb03e08544788f990e85f21e7a073e
Hareram Singh <hareram.singh@netcracker.com>
2025-08-25 23:04:43 +0530

    latest

--- LinkedList/src/com/hs/hard/MergeKSortedLinkedList.java
+++ LinkedList/src/com/hs/hard/MergeKSortedLinkedList.java
@@ -21,10 +21,10 @@ public class MergeKSortedLinkedList {
 		ListNode left = mergeKListsHelper(lists, start, mid);
 		ListNode right = mergeKListsHelper(lists, mid + 1, end);
 
-		return mergeTwoLists(left, right);
+		return merge(left, right);
 	}
 
-	private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+	private ListNode merge(ListNode list1, ListNode list2) {
 		if (list1 == null)
 			return list2;
 
@@ -32,10 +32,10 @@ public class MergeKSortedLinkedList {
 			return list1;
 
 		if (list1.val < list2.val) {
-			list1.next = mergeTwoLists(list1.next, list2);
+			list1.next = merge(list1.next, list2);
 			return list1;
 		} else {
-			list2.next = mergeTwoLists(list1, list2.next);
+			list2.next = merge(list1, list2.next);
 			return list2;
 		}
 	}
--- LinkedList/src/com/hs/medium/ReorderList.java
+++ LinkedList/src/com/hs/medium/ReorderList.java
@@ -20,11 +20,14 @@ public class ReorderList {
 
 	private void merge(ListNode l1, ListNode l2) {
 		while (l1 != null && l2 != null) {
-			ListNode next = l1.next;
-			l1.next = l2;
-			l1 = l2;
-			l2 = next;
-		}
+	        ListNode l1Next = l1.next;
+	        ListNode l2Next = l2.next;
+
+	        l1.next = l2;           // attach node from l2
+	        l2.next = l1Next;       // attach next from l1
+	        l1 = l1Next;            // move forward in l1
+	        l2 = l2Next;            // move forward in l2
+	    }
 	}
 
 	public static void main(String[] args) {
